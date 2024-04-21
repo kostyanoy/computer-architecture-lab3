9f099859ab1ebafe2ed43784f7fa8e54082ad75c
Kosty Manuhin <kostymanuhin@gmail.com>
2024-04-22 01:11:03 +0300

    golden tests

--- golden_test.py
+++ golden_test.py
@@ -6,15 +6,24 @@ import tempfile
 
 import pytest
 
-import machine
+import simulation
 import translator
-from isa import read_code
+from machine.isa import read_code
 
 
-# TODO Ci gitlab
-# TODO адресация портов ??
-@pytest.mark.golden_test("golden/*_full.yml")
-def test_translator_and_machine(golden, caplog):
+def main_test(
+    golden,
+    caplog,
+    STACK_SIZE=10,
+    DEBUG_LIMIT=200,
+    LIMIT=100000,
+    CACHE_SIZE=8,
+    BUS_SIZE=8,
+    DELAY_TICKS=1,
+    STACK_LOG=False,
+    CACHE_LOG=False,
+    INST_FETCH_LOG=False,
+):
     """
     Test: poetry run pytest . -v
     Update tests: poetry run pytest . -v --update-goldens
@@ -40,7 +49,19 @@ def test_translator_and_machine(golden, caplog):
         with contextlib.redirect_stdout(io.StringIO()) as stdout:
             translator.main(source, target, target_mnem)
             print("============================================================")
-            machine.main(target, input_stream)
+            simulation.main(
+                target,
+                input_stream,
+                STACK_SIZE,
+                DEBUG_LIMIT,
+                LIMIT,
+                CACHE_SIZE,
+                BUS_SIZE,
+                DELAY_TICKS,
+                STACK_LOG,
+                CACHE_LOG,
+                INST_FETCH_LOG,
+            )
 
         # Выходные данные также считываем в переменные.
         code = read_code(target)
@@ -54,42 +75,109 @@ def test_translator_and_machine(golden, caplog):
         assert caplog.text == golden.out["out_log"]
 
 
-@pytest.mark.golden_test("golden/*_short.yml")
-def test_translator_and_machine_short(golden, caplog):
-    """
-    Test: poetry poetry run pytest . -v
-    Update tests: poetry run pytest . -v --update-goldens
-    """
-    caplog.set_level(logging.DEBUG)
+@pytest.mark.golden_test("golden/*_alg.yml")
+def test_translator_and_machine(golden, caplog):
+    STACK_SIZE = 10
+    DEBUG_LIMIT = 200
+    LIMIT = 100000
+    CACHE_SIZE = 8
+    BUS_SIZE = 8
+    DELAY_TICKS = 1
+    STACK_LOG = False
+    CACHE_LOG = False
+    INST_FETCH_LOG = False
 
-    # Создаём временную папку для тестирования приложения.
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        # Готовим имена файлов для входных и выходных данных.
-        source = os.path.join(tmpdirname, "source.txt")
-        input_stream = os.path.join(tmpdirname, "input.txt")
-        target = os.path.join(tmpdirname, "target.out")
-        target_mnem = os.path.join(tmpdirname, "target.out.mnem")
+    main_test(
+        golden,
+        caplog,
+        STACK_SIZE,
+        DEBUG_LIMIT,
+        LIMIT,
+        CACHE_SIZE,
+        BUS_SIZE,
+        DELAY_TICKS,
+        STACK_LOG,
+        CACHE_LOG,
+        INST_FETCH_LOG,
+    )
 
-        # Записываем входные данные в файлы. Данные берутся из теста.
-        with open(source, "w") as file:
-            file.write(golden["in_source"])
-        with open(input_stream, "w") as file:
-            file.write(golden["in_stdin"])
 
-        # Запускаем транслятор и собираем весь стандартный вывод в переменную
-        # stdout
-        with contextlib.redirect_stdout(io.StringIO()) as stdout:
-            translator.main(source, target, target_mnem)
-            print("============================================================")
-            machine.main(target, input_stream, False)
+@pytest.mark.golden_test("golden/*_cache_demonstr.yml")
+def test_cache_demonstr(golden, caplog):
+    STACK_SIZE = 10
+    DEBUG_LIMIT = 200
+    LIMIT = 100000
+    CACHE_SIZE = 8
+    BUS_SIZE = 8
+    DELAY_TICKS = 10
+    STACK_LOG = False
+    CACHE_LOG = True
+    INST_FETCH_LOG = False
 
-        # Выходные данные также считываем в переменные.
-        code = read_code(target)
-        with open(target_mnem, "r") as f:
-            mnemonics = f.read()
+    main_test(
+        golden,
+        caplog,
+        STACK_SIZE,
+        DEBUG_LIMIT,
+        LIMIT,
+        CACHE_SIZE,
+        BUS_SIZE,
+        DELAY_TICKS,
+        STACK_LOG,
+        CACHE_LOG,
+        INST_FETCH_LOG,
+    )
 
-        # Проверяем, что ожидания соответствуют реальности.
-        assert mnemonics == golden.out["out_mnemonics"]
-        assert code == golden.out["out_code"]
-        assert stdout.getvalue() == golden.out["out_stdout"]
-        assert caplog.text == golden.out["out_log"]
+
+@pytest.mark.golden_test("golden/*_small_cache.yml")
+def test_small_cache(golden, caplog):
+    STACK_SIZE = 10
+    DEBUG_LIMIT = 200
+    LIMIT = 100000
+    CACHE_SIZE = 2
+    BUS_SIZE = 2
+    DELAY_TICKS = 10
+    STACK_LOG = False
+    CACHE_LOG = True
+    INST_FETCH_LOG = False
+
+    main_test(
+        golden,
+        caplog,
+        STACK_SIZE,
+        DEBUG_LIMIT,
+        LIMIT,
+        CACHE_SIZE,
+        BUS_SIZE,
+        DELAY_TICKS,
+        STACK_LOG,
+        CACHE_LOG,
+        INST_FETCH_LOG,
+    )
+
+
+@pytest.mark.golden_test("golden/*_small_bus.yml")
+def test_small_bus(golden, caplog):
+    STACK_SIZE = 10
+    DEBUG_LIMIT = 200
+    LIMIT = 100000
+    CACHE_SIZE = 8
+    BUS_SIZE = 1
+    DELAY_TICKS = 10
+    STACK_LOG = False
+    CACHE_LOG = True
+    INST_FETCH_LOG = False
+
+    main_test(
+        golden,
+        caplog,
+        STACK_SIZE,
+        DEBUG_LIMIT,
+        LIMIT,
+        CACHE_SIZE,
+        BUS_SIZE,
+        DELAY_TICKS,
+        STACK_LOG,
+        CACHE_LOG,
+        INST_FETCH_LOG,
+    )
